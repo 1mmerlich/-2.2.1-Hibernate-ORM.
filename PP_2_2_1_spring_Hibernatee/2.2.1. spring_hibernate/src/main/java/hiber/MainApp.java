31a351064a67ba11ac3bd4db1e080999ceb6a3d1
1mmerlich <144177282+1mmerlich@users.noreply.github.com>
2024-06-17 17:26:30 +0300

    Add files via upload

--- PP_2_2_1_spring_Hibernatee/2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ PP_2_2_1_spring_Hibernatee/2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -13,6 +13,14 @@ public class MainApp {
    public static void main(String[] args) throws SQLException {
       AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
       UserService userService = context.getBean(UserService.class);
+      Car car1 = new Car("ВАЗ", 2108 );
+      Car car2 = new Car("ВАЗ", 2109 );
+      Car car3 = new Car("ВАЗ", 2110 );
+      Car car4 = new Car("ВАЗ", 2111 );
+      userService.add(new User("User1", "Lastname1", "user1@mail.ru", car1));
+      userService.add(new User("User2", "Lastname2", "user2@mail.ru", car2));
+      userService.add(new User("User3", "Lastname3", "user3@mail.ru", car3));
+      userService.add(new User("User4", "Lastname4", "user4@mail.ru", car4));
 
       List<User> users = userService.listUsers();
 
